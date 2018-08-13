6bb6169f5f53d4e5092acc224d933665161f2051
Nate Cheng <boo0330@gmail.com>
2018-08-14 06:46:33 +0800

    add custom range examples

--- playground/app/app.component.ts
+++ playground/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,35 @@ export class AppComponent {
   end2;
   start3;
   end3;
+
+  customRanges = [
+    {
+      text: 'Today', desc: 'Today', value: 'today',
+      start: moment(),
+      end: moment(),
+      default: true
+    },
+    {
+      text: 'Last 3 Months', desc: 'Last 3 Months', value: 'last3month',
+      start: moment().subtract(3, 'month').startOf('month'),
+      end: moment().subtract(1, 'month').endOf('month')
+    },
+    {
+      text: 'Last 6 Month', desc: 'Last Month', value: 'lastmonth',
+      start: moment().subtract(6, 'month').startOf('month'),
+      end: moment().subtract(1, 'month').endOf('month')
+    },
+    {
+      text: 'This Year', desc: 'This Year', value: 'thisyear',
+      start: moment().startOf('year'),
+      end: moment().endOf('year')
+    },
+    {
+      text: 'Last Year', desc: 'Last Year', value: 'lastyear',
+      start: moment().subtract(1, 'year').startOf('year'),
+      end: moment().subtract(1, 'year').endOf('year')
+    }
+  ];
 
   constructor() {
   }
